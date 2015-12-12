5b656edf731bb6fcb29c4bb8c9d119d2bf706a2a
Piotr Pupczyk <ppsirius@gmail.com>
2015-12-12 22:29:10 +0100

    - smooth max heihgt animiation with gsap

--- js/main.js
+++ js/main.js
@@ -6,14 +6,39 @@ $(function() {
     //
     //}
 
-
+    // Mobile navigation
     $('.hamburger').click(function(event){
         $('.mobile-menu').addClass('open');
     });
-
     $('.close-menu').click(function(event){
         $('.mobile-menu').removeClass('open');
     });
+
+    // Max height animation
+    function animatedMaxHeight(value) {
+        var listWrapper = $(value).next(),
+            listMaxHeight = listWrapper.children('ul').height();
+
+        if ( listWrapper.css('max-height') == '0px' ) {
+            TweenLite.to(listWrapper, .3, {css: {maxHeight: listMaxHeight}})
+        } else {
+            listWrapper.css('max-height', listMaxHeight);
+            TweenLite.to(listWrapper, .3, {css: {maxHeight: 0}})
+        }
+    }
+
+    // Filter sidebar
+    $('.filter-title').click(function(){
+        animatedMaxHeight(this);
+    });
+
+    // Footer dropdown
+    $('.footer-headline').click(function(event){
+        if(mediaQuery != 'desktop') {
+            animatedMaxHeight(this);
+        }
+    });
+
 
 
 });
